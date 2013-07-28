28dd9ba1bcdc8658954693b05b972026f81c712b
Mahmut Bulut <mahmutbulut@icloud.com>
2013-07-28 01:25:28 +0000

    static build, name mangling

--- wiringpi-build.sh
+++ wiringpi-build.sh
@@ -5,15 +5,33 @@ if [ "$(id -u)" != "0" ]; then
    exit 1
 fi
 
-echo "Wiring pi lib build starting..."
-echo "===================================="
-echo
-
 git clone git://git.drogon.net/wiringPi
-echo && echo
-cd wiringPi && ./build
 
-if [ $? == 0 ]; then
+do_build(){
+	echo "Select build type: " && echo "Static(s)" && echo "Dynamic(D) ?" && echo
+	read r
+	if [ $r = "D" ]; then
+		echo "Wiring pi lib build starting..."
+		echo "===================================="
+		echo && echo
+		cd wiringPi && ./build
+	else
+		if [ $r = "s" ]; then
+			echo "Wiring pi lib build starting..."
+			echo "===================================="
+			echo && echo
+			cd wiringPi && cd wiringPi
+			make static
+			sudo make install-static
+		else
+			do_build
+		fi
+	fi
+}
+
+do_build
+	
+if [ $? = 0 ]; then
 	echo "Successfully installed."
 else
 	echo "An error occured during the installation."
